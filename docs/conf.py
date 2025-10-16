9e8179f1d7f04ce6e172e5bc6cb043ccbe71762d
Dmitrii Kochkov <dkochkov@google.com>
2025-10-16 11:55:56 -0700

    Added initial version of intro docs for the experimental API.
    PiperOrigin-RevId: 820335148

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,13 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.linkcode',
     'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
     'myst_nb',
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
